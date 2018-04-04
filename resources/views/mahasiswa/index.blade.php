8d765bd7114d36fbd93d5c8deff28ce47b332d87
Roland Hartanto <rolandhartanto@gmail.com>
2018-04-04 14:35:58 +0700

    add conditional handling for mahasiswa status bar

--- resources/views/mahasiswa/index.blade.php
+++ resources/views/mahasiswa/index.blade.php
@@ -2,6 +2,7 @@
 @section('title')Dashboard Mahasiswa @endsection
 @php
     $user = Auth::user();
+    $mahasiswa = Auth::user()->isMahasiswa();
 @endphp
 @section('content')
     <div class="container dashboard-mahasiswa">
@@ -9,20 +10,65 @@
 
         <div class="progress_containter">
             <div class="level_progress">
-                <div class="progress progress_1">
-                    <div class="bar done"></div>
-                </div>
-                <div class="progress progress_2">
-                    <div class="bar"></div>
-                </div>
+                @if($mahasiswa->status >= 4)
+                    <div class="progress progress_1">
+                        <div class="bar done"></div>
+                    </div>
+                @elseif($mahasiswa->status >=2)
+                    <div class="progress progress_1">
+                        <div class="bar prog60"></div>
+                    </div>
+                @elseif($mahasiswa->status >=1)
+                    <div class="progress progress_1">
+                        <div class="bar prog30"></div>
+                    </div>
+                @else
+                    <div class="progress progress_1">
+                        <div class="bar"></div>
+                    </div>
+                @endif
+
+                @if($mahasiswa->status >= 9)
+                    <div class="progress progress_2">
+                        <div class="bar done"></div>
+                    </div>
+                @elseif($mahasiswa->status >=7)
+                    <div class="progress progress_2">
+                        <div class="bar prog60"></div>
+                    </div>
+                @elseif($mahasiswa->status >= 6)
+                    <div class="progress progress_2">
+                        <div class="bar prog30"></div>
+                    </div>
+                @else
+                    <div class="progress progress_2">
+                        <div class="bar"></div>
+                    </div>
+                @endif
                 <div class="progress progress_3">
                     <div class="bar"></div>
                 </div>
 
+
                 <div class="level level_1 level_reached"><p>1</p></div>
-                <div class="level level_2 level_reached"><p>2</p></div>
-                <div class="level level_3"><p>3</p></div>
-                <div class="level level_4"><p>4</p></div>
+
+                @if($mahasiswa->status >= 5)
+                    <div class="level level_2 reached"><p>2</p></div>
+                @else
+                    <div class="level level_2"><p>2</p></div>
+                @endif
+
+                @if($mahasiswa->status >= 10)
+                    <div class="level level_3 reached"><p>2</p></div>
+                @else
+                    <div class="level level_3"><p>2</p></div>
+                @endif
+
+                @if($mahasiswa->status >= 14)
+                    <div class="level level_4 reached"><p>2</p></div>
+                @else
+                    <div class="level level_4"><p>2</p></div>
+                @endif
 
                 <div class="nav" role="tablist">
                     <a class="nav-link" data-toggle="tab" href="#step1">
@@ -50,8 +96,9 @@
         </div>
 
         <div class="tab-content">
-            <div id="step1" class="container tab-pane fade">
+            <div id="step1" class="container tab-pane fade active show">
                 <h3 class="header">Seminar Topik</h3>
+                
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
             </div>
             <div id="step2" class="container tab-pane fade">
@@ -76,4 +123,9 @@
             </div>
         </div-->
     </div>
+@endsection
+@section('bottomjs')
+    <script>
+
+    </script>
 @endsection
