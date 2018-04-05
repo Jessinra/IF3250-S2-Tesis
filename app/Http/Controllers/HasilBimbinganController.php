faa51b48ef4d5fc4caac3624fca0146e74dcc20b
Roland Hartanto <rolandhartanto@gmail.com>
2018-04-05 14:20:58 +0700

    create hasil bimbingan controller

---
+++ app/Http/Controllers/HasilBimbinganController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\HasilBimbingan;
+use Illuminate\Http\Request;
+
+class HasilBimbinganController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\HasilBimbingan  $hasilBimbingan
+     * @return \Illuminate\Http\Response
+     */
+    public function show(HasilBimbingan $hasilBimbingan)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\HasilBimbingan  $hasilBimbingan
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(HasilBimbingan $hasilBimbingan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\HasilBimbingan  $hasilBimbingan
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, HasilBimbingan $hasilBimbingan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\HasilBimbingan  $hasilBimbingan
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(HasilBimbingan $hasilBimbingan)
+    {
+        //
+    }
+}
