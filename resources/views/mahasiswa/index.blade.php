9fd28858352479194b352e5456046eb841abe692
Roland Hartanto <rolandhartanto@gmail.com>
2018-04-05 21:46:46 +0700

    add navigation to add hasil bimbingan

--- resources/views/mahasiswa/index.blade.php
+++ resources/views/mahasiswa/index.blade.php
@@ -336,7 +336,10 @@
             <div id="step3" class="container tab-pane fade">
         @endif
                 <h3 class="header">Seminar Tesis</h3>
-                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+                <p>Anda dapat mengunggah hasil bimbingan setiap kali selesai bimbingan.</p>
+                <a class="btn btn-blue" href="/hasilbimbingan/tambah" role="button">Entri Hasil Bimbingan</a>
+                <a class="btn btn-outline-dark" href="/hasilbimbingan" role="button">Lihat Hasil Bimbingan</a>
+
             </div>
         @if($mahasiswa->status >= \App\Mahasiswa::STATUS_LULUS_SEMINAR_TESIS)
             <div id="step4" class="container tab-pane fade active show">
