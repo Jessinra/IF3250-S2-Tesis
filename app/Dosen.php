d454e4ef2f385eb988ee9522ac27db88aa47c6f5
Roland Hartanto <rolandhartanto@gmail.com>
2018-04-10 01:07:42 +0700

    fix db query for retriving user's username and name

--- app/Dosen.php
+++ app/Dosen.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Dosen extends Model
 {
@@ -20,7 +21,12 @@ class Dosen extends Model
     }
 
     public function getHasilBimbingan(){
-        $hsl_bimbingan = HasilBimbingan::where('dosen_id',$this->id)->get();
+        //$hsl_bimbingan = HasilBimbingan::where('dosen_id',$this->id)->get();
+        $hsl_bimbingan = HasilBimbingan::join('users', 'mahasiswa_id', '=', 'users.id')
+                            ->where('dosen_id',$this->id)
+                            ->orderBy('tanggal_waktu','desc')->orderBy('status','asc')
+                            ->select('hasil_bimbingans.id', 'mahasiswa_id', 'dosen_id', 'status', 'tanggal_waktu', 'topik', 'hasil_dan_diskusi', 'rencana_tindak_lanjut', 'users.name', 'users.username')
+                            ->get();
         return $hsl_bimbingan;
     }
 }
