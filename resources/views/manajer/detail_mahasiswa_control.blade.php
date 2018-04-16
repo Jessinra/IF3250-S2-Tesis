e97a245f1c18a477051791787f4a18b62dd578ad
Kenneth Halim <kanisiuskenneth@gmail.com>
2018-04-16 12:36:05 +0700

    Add table hasil bimbingan in detail mahasiswa view

--- resources/views/manajer/detail_mahasiswa_control.blade.php
+++ resources/views/manajer/detail_mahasiswa_control.blade.php
@@ -48,7 +48,84 @@
                             Masa Bimbingan
                         </h3>
                     </div>
-                    {{$hasilBimbinganAktif}}
+                    <table class="mahasiswa-control-table width-full table table-hover">
+                        <thead>
+                        <tr class="text-center">
+                            <th>
+                                No
+                            </th>
+                            <th>
+                                Topik
+                            </th>
+                            <th>
+                                Waktu Bimbingan
+                            </th>
+                            <th>
+                                Status
+                            </th>
+                            <th></th>
+                        </tr>
+                        </thead>
+
+                        @foreach($hasilBimbinganAktif as $item)
+                            <tr class="text-center">
+                                <td>
+                                    {{$loop->iteration}}
+                                </td>
+                                <td>
+                                    {{$item->topik}}
+                                </td>
+                                <td>
+                                    {{$item->tanggal_waktu}}
+                                </td>
+                                <td>
+                                    {{$item->getStatusString()}}
+                                </td>
+
+                                <td class="row">
+                                    <button class="btn btn-icon display-flex justify-content-center align-items-center" data-toggle="modal" data-target="#hsl{{$loop->iteration}}">
+                                        <i class="material-icons font-size-18-px">
+                                            search
+                                        </i>
+                                    </button>
+
+                                    <div class="modal fade" id="hsl{{$loop->iteration}}">
+                                        <div class="modal-dialog">
+                                            <div class="modal-content">
+
+                                                <!-- Modal Header -->
+                                                <div class="modal-header">
+                                                    <h4 class="modal-title">Topik: {{$item->topik}}</h4>
+                                                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                                </div>
+
+                                                <!-- Modal body -->
+                                                <div class="modal-body">
+                                                    <h5>Dosen Pembimbing: </h5>
+                                                    <p>{{$item->name}}</p>
+                                                    <h5>Waktu Bimbingan: </h5>
+                                                    <p>{{$item->tanggal_waktu}}</p>
+                                                    <h5>Hasil dan diskusi: </h5>
+                                                    <p>{{$item->hasil_dan_diskusi}}</p>
+                                                    <h5>Rencana tindak lanjut:</h5>
+                                                    <p>{{$item->rencana_tindak_lanjut}}</p>
+
+                                                </div>
+
+                                                <!-- Modal footer -->
+                                                <div class="modal-footer">
+                                                    <button type="submit" class="btn btn-danger" data-dismiss="modal">Tutup</button>
+                                                </div>
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                    
+                                </td>
+
+                            </tr>
+                        @endforeach
+                    </table>
                 @endif
                 @if($mahasiswa->status >= \App\Mahasiswa::STATUS_LULUS_SEMINAR_PROPOSAL || $mahasiswa->status < \App\Mahasiswa::STATUS_GAGAL_SEMINAR_PROPOSAL )
 
