93ba8393945815ac8b8676d1223fa5bca13994fc
Roland Hartanto <rolandhartanto@gmail.com>
2018-04-17 11:27:27 +0700

    add new column to hasil bimbingan

--- app/HasilBimbingan.php
+++ app/HasilBimbingan.php
@@ -17,14 +17,18 @@ class HasilBimbingan extends Model
     ];
 
     protected $fillable = [
-        'id','mahasiswa_id','dosen_id','status','tanggal_waktu','topik','hasil_dan_diskusi','rencana_tindak_lanjut'];
+        'id','mahasiswa_id','dosen_id','status','tanggal_waktu','topik','hasil_dan_diskusi','rencana_tindak_lanjut', 'dosen_id2', 'waktu_bimbingan_selanjutnya'];
 
     public function dosen_pembimbing() {
         return $this->belongsTo('App\Dosen', 'dosen_id', 'id');
     }
 
+    public function dosen_pembimbing2() {
+        return $this->belongsTo('App\Dosen', 'dosen_id2', 'id');
+    }
+
     public function mahasiswa() {
-        return $this->belongsTo('App\Mahasiswa', 'mahasiswa_id', 'id');
+        return $this->belongsTo('App\Thesis', 'mahasiswa_id', 'id');
     }
 
     public function getStatusString() {
