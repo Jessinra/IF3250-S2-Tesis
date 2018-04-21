a47cc4488a5aa394d51b2eac98b6b089ef8deeb1
radiyyadwi <radiyyasaputra@gmail.com>
2018-04-21 21:07:49 +0700

    add attribute fillable in model sidang tesis

--- app/SidangTesis.php
+++ app/SidangTesis.php
@@ -8,7 +8,7 @@ class SidangTesis extends Model
 {
     protected $table = 'sidang_tesis';
     protected $fillable =
-        ['thesis_id','id','nilai_dosen_penguji_1_utama'.'nilai_dosen_penguji_2_utama','nilai_dosen_pembimbing_utama','nilai_dosen_penguji_1_penting'.'nilai_dosen_penguji_2_penting','nilai_dosen_pembimbing_penting','nilai_dosen_penguji_1_pendukung'.'nilai_dosen_penguji_2_pendukung','nilai_dosen_pembimbing_pendukung','nilai_dosen_kelas_penting','nilai_dosen_kelas_utama'];
+        ['thesis_id','id','nilai','waktu','dosen_penguji_1','dosen_penguji_2','nilai_dosen_penguji_1_utama'.'nilai_dosen_penguji_2_utama','nilai_dosen_pembimbing_utama','nilai_dosen_penguji_1_penting'.'nilai_dosen_penguji_2_penting','nilai_dosen_pembimbing_penting','nilai_dosen_penguji_1_pendukung'.'nilai_dosen_penguji_2_pendukung','nilai_dosen_pembimbing_pendukung','nilai_dosen_kelas_penting','nilai_dosen_kelas_utama'];
 
     public function tesis() {
         return $this->belongsTo('App\Thesis', 'thesis_id','id');
@@ -22,4 +22,6 @@ class SidangTesis extends Model
     public function dosen_penguji_2() {
         return $this->belongsTo('App\User','dosen_penguji_2','id');
     }
+
+    
 }
