1dcd4f86ef83e03221cb566b16b75eb3892229d5
Roland Hartanto <rolandhartanto@gmail.com>
2018-04-25 22:47:54 +0700

    add unit testing for login and register

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use Illuminate\Support\Facades\Session;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -14,6 +15,35 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $this->assertTrue(true);
+        //$this->assertTrue(true);
+        //register
+        $response = $this->get('/register');
+        $response->assertStatus(403);
+
+        //login test
+        $response = $this->get('/login');
+        $response->assertStatus(200);
+
+        $username = 'admin';
+        $password = 'admin';
+        Session::start();
+        $response = $this->followingRedirects()->call('POST', '/login', [
+            'username' => $username,
+            'password' => $password,
+            '_token' => csrf_token()
+        ]);
+
+        $this->assertEquals(200, $response->getStatusCode());
+
+        //register admin
+        $response = $this->get('/register');
+        $response->assertStatus(200);
+
+        $response = $this->followingRedirects()->call('POST', '/logout',[
+            '_token' => csrf_token()
+        ]);
+
+        $this->assertEquals(200, $response->getStatusCode());
+
     }
 }
